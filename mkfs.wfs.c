bec52e8d126f1d9ebf28f48b10503c195551bbbb
MikeHarrington15 <85142498+MikeHarrington15@users.noreply.github.com>
2023-12-05 16:47:54 -0600

    Add files via upload

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 #include "wfs.h"
+
+#define DISK_SIZE 1048576  // Example size, e.g., 1MB
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -12,16 +15,23 @@ int main(int argc, char *argv[]) {
     }
 
     const char *disk_path = argv[1];
-    int fd = open(disk_path, O_RDWR | O_CREAT, 0666);
+    int fd = open(disk_path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd == -1) {
-        perror("Error opening disk file");
+        perror("Error opening or creating disk file");
+        return EXIT_FAILURE;
+    }
+
+    // Ensure the disk is of the correct size (e.g., 1MB)
+    if (ftruncate(fd, DISK_SIZE) == -1) {
+        perror("Error setting disk size");
+        close(fd);
         return EXIT_FAILURE;
     }
 
     // Initialize the superblock
     struct wfs_sb superblock;
     superblock.magic = WFS_MAGIC;
-    superblock.head = sizeof(struct wfs_sb);  // The next free space starts right after the superblock
+    superblock.head = sizeof(struct wfs_sb);
 
     // Write the superblock to the disk image
     if (write(fd, &superblock, sizeof(superblock)) != sizeof(superblock)) {
@@ -30,21 +40,33 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    // Initialize the root directory log entry
     struct wfs_log_entry emptyDirectory;
     struct wfs_inode emptyDirectoryInode;
-    emptyDirectory.inode = emptyDirectoryInode;
-    //inode 
+    
     emptyDirectoryInode.mode = S_IFDIR | 0755;
     emptyDirectoryInode.inode_number = 0;
-    emptyDirectoryInode.links = 1;
+    emptyDirectoryInode.links = 2;  // Typically 2 for directories (".", "..")
     emptyDirectoryInode.uid = getuid();
     emptyDirectoryInode.gid = getgid();
     emptyDirectoryInode.atime = time(NULL);
     emptyDirectoryInode.mtime = time(NULL);
     emptyDirectoryInode.ctime = time(NULL);
-    emptyDirectoryInode.size = 0;
+    emptyDirectoryInode.size = 0;  // No entries yet
 
-    //how to add it to the file system?
+    emptyDirectory.inode = emptyDirectoryInode;
+
+    // Write the root directory log entry to the disk
+    if (write(fd, &emptyDirectory, sizeof(struct wfs_inode)) != sizeof(struct wfs_inode)) {
+        perror("Error writing root directory log entry");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    // Update the superblock to point to the next free space
+    superblock.head += sizeof(struct wfs_inode);  // Update to the next free space
+    lseek(fd, 0, SEEK_SET);  // Go back to the beginning of the file
+    write(fd, &superblock, sizeof(superblock));  // Write the updated superblock
 
     close(fd);
     return EXIT_SUCCESS;
