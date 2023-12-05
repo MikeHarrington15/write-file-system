c75a794d80b8b5dd681213cfa8d08a418b87acde
Ben Lynch <96927431+benl17@users.noreply.github.com>
2023-12-05 16:36:08 -0600

    Add files via upload
    mkfs.wfs.c changed

--- mkfs.wfs.c
+++ mkfs.wfs.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     }
 
     const char *disk_path = argv[1];
-    int fd = open(disk_path, O_RDWR);
+    int fd = open(disk_path, O_RDWR | O_CREAT, 0666);
     if (fd == -1) {
         perror("Error opening disk file");
         return EXIT_FAILURE;
@@ -29,6 +29,22 @@ int main(int argc, char *argv[]) {
         close(fd);
         return EXIT_FAILURE;
     }
+
+    struct wfs_log_entry emptyDirectory;
+    struct wfs_inode emptyDirectoryInode;
+    emptyDirectory.inode = emptyDirectoryInode;
+    //inode 
+    emptyDirectoryInode.mode = S_IFDIR | 0755;
+    emptyDirectoryInode.inode_number = 0;
+    emptyDirectoryInode.links = 1;
+    emptyDirectoryInode.uid = getuid();
+    emptyDirectoryInode.gid = getgid();
+    emptyDirectoryInode.atime = time(NULL);
+    emptyDirectoryInode.mtime = time(NULL);
+    emptyDirectoryInode.ctime = time(NULL);
+    emptyDirectoryInode.size = 0;
+
+    //how to add it to the file system?
 
     close(fd);
     return EXIT_SUCCESS;
