6cf80ab1b65003f28064271719b3f816b2d99f8a
MikeHarrington15 <85142498+MikeHarrington15@users.noreply.github.com>
2023-12-08 10:21:34 -0600

    Add files via upload

--- mount.wfs.c
+++ mount.wfs.c
@@ -26,7 +26,7 @@ static int wfs_unlink(const char *path);
 
 // Global file descriptor for the disk file
 int disk_fd = -1;
-int newInode = 0;
+int newInode = 5; // arbitrary number that is not 0 or 1
 struct wfs_sb *global_superblock = NULL;
 
 // Initialize fuse_operations structure
@@ -40,8 +40,38 @@ static struct fuse_operations ops = {
     .unlink    	= wfs_unlink,
 };
 
+struct wfs_log_entry* find_entry_by_inode(int inode) {
+    struct wfs_log_entry *start_of_log = (struct wfs_log_entry *)((char *)global_superblock + sizeof(struct wfs_sb));
+    struct wfs_log_entry *end_of_log = (struct wfs_log_entry *)((char *)global_superblock + global_superblock->head);
+    struct wfs_log_entry *current_entry = start_of_log;
+    struct wfs_log_entry *found_entry = NULL;
+
+    int found = 0;
+
+    printf("find_entry_by_inode: %d\n", inode);
+    while (current_entry < end_of_log) {
+        // Check if the current entry is a directory or file with the correct name and inode
+        if ((current_entry->inode.inode_number == inode)) {
+            
+            found_entry = current_entry;
+            found = 1;
+        }
+        // Move to the next log entry
+        current_entry = (struct wfs_log_entry *)((char *)current_entry + sizeof(struct wfs_log_entry) + current_entry->inode.size);
+    }
+
+    if (!found || found_entry->inode.deleted == 1) { // also check if deleted
+        // Token not found in the log, path  does not exist
+        printf("Inode %d not found or deleted\n", inode);
+        return NULL;
+    }
+
+    printf("Found entry by inode %d\n", found_entry->inode.inode_number);
+    return found_entry;
+}
+
 struct wfs_log_entry* looper(const char *path, mode_t mode) {
-    printf("Looper path search: %s\n", path);
+    printf("Get looper: %s\n", path);
     // Check if the global superblock has been mapped
     if (!global_superblock) {
         fprintf(stderr, "Disk file not mapped\n");
@@ -62,15 +92,20 @@ struct wfs_log_entry* looper(const char *path, mode_t mode) {
         char *rest = strdup(path);
         token = rest;
     }
+
     unsigned int current_inode = 0; // Start with root inode    
+
+    printf("%s search start token: %s\n", path, token);
 
     while (token != NULL) {
         current_entry = start_of_log;
         int found = 0;
 
+        printf("%s searching in looper\n", path);
         while (current_entry < end_of_log) {
             // Check if the current entry is a directory or file with the correct name and inode
             if ((current_entry->inode.inode_number == current_inode)) {
+                
                 found_entry = current_entry;
                 found = 1;
             }
@@ -80,43 +115,60 @@ struct wfs_log_entry* looper(const char *path, mode_t mode) {
 
         if (!found || found_entry->inode.deleted == 1) { // also check if deleted
             // Token not found in the log, path  does not exist
+            printf("%s Not found in looper\n", path);
             return NULL;
         }
 
         // get found_entry
         //////--------------------///////////////////
         // get current_inode
-
-        struct wfs_dentry * current_dentry = (struct wfs_dentry *)found_entry->data;
+        struct wfs_dentry *current_dentry = (struct wfs_dentry *)found_entry->data;
         int num_entry = found_entry->inode.size / sizeof(struct wfs_dentry);
 
+        printf("Listing all dentries in found_entry:\n");
         for (int i = 0; i < num_entry; i++) {
-            if (current_dentry->name == token) {
+            printf("Dentry %d: Name: %s, Inode Number: %lu\n", i, current_dentry->name, current_dentry->inode_number);
+            current_dentry += 1;
+        }
+
+        // Reset current_dentry to the start position before the main loop
+        current_dentry = (struct wfs_dentry *)found_entry->data;
+        int old_inode = current_inode;
+
+        printf("%s before with name: %s token: %s\n", path, current_dentry->name, token);
+        for (int i = 0; i < num_entry; i++) {
+            
+            if (strcmp(current_dentry->name, token) == 0) {
+                printf("%s made it in here with name: %s token: %s\n", path, current_dentry->name, token);
                 current_inode = current_dentry->inode_number;
             }
             current_dentry += 1;
         }
 
         // Update inode for the next path component
-        current_inode = found_entry->inode.inode_number;
+        if (current_inode == old_inode && strcmp(path, "/") != 0) {
+            printf("Returning current = old\n");
+            return NULL;
+        }
 
-        if (strcmp(path, "/") != 0) {
+        if (strcmp(path, "/") == 0) {
             break;
         }
 
         token = strtok(NULL, "/"); // Move to next token
     }
-
-    return found_entry; // Return the pointer to the last found entry
+    printf("Found entry: %d\n", found_entry->inode.inode_number);
+    
+    return find_entry_by_inode(current_inode);; // Return the pointer to the last found entry
 }
 
 static int wfs_getattr(const char *path, struct stat *stbuf) {
+    printf("Get path: %s\n", path);
     memset(stbuf, 0, sizeof(struct stat)); // Initialize the stat structure
 
     struct wfs_log_entry *entry = looper(path, 0); // Use looper to find the log entry for the path
-    printf("Entry: %p\n", (void *)entry);
     if (!entry) {
-        printf("Entering no entry\n");
+        printf("Fails here for path: %s\n", path);
         return -ENOENT; // File or directory not found
     }
 
@@ -126,39 +178,66 @@ static int wfs_getattr(const char *path, struct stat *stbuf) {
     stbuf->st_uid = entry->inode.uid;
     stbuf->st_gid = entry->inode.gid;
     stbuf->st_size = entry->inode.size;
-    stbuf->st_atime = entry->inode.atime;
     stbuf->st_mtime = entry->inode.mtime;
-    stbuf->st_ctime = entry->inode.ctime;
-    stbuf->st_ino = entry->inode.inode_number;
 
     return 0; // Return success
 }
 
 static int wfs_mknod(const char *path, mode_t mode, dev_t rdev) {
+    printf("Entering mknod path: %s *********************************\n", path);
     // Check if superblock is properly mapped
     if (!global_superblock) {
         fprintf(stderr, "Superblock not mapped\n");
         return -EIO;
     }
 
-    // Use looper to find the parent directory of the path
     char parent_path[MAX_PATH_LEN];
+
+    if (strcmp(path, "/") == 0) {
+        // Path is the root directory, no parent
+        return -EISDIR;
+    }
+
     strcpy(parent_path, path);
     char *last_slash = strrchr(parent_path, '/');
-    if (last_slash == NULL) {
-        return -ENOENT;  // Invalid path
+
+    char new_path[MAX_FILE_NAME_LEN];
+
+    if (last_slash != NULL) {
+        // Copy everything after the last slash to new_path
+        strncpy(new_path, last_slash + 1, MAX_FILE_NAME_LEN); // Copy up to 4 characters to leave room for the null terminator
+        new_path[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
+    } else {
+        // If there's no slash, the whole path is the new path
+        strncpy(new_path, path, MAX_FILE_NAME_LEN);
+        new_path[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
     }
-    *last_slash = '\0'; // Terminate the string to get the parent path
+
+    if (last_slash == parent_path) {
+        // If the path is a direct child of root (e.g., "/a")
+        strcpy(parent_path, "/");
+    } else if (last_slash != NULL) {
+        // For deeper paths, find the parent directory
+        *last_slash = '\0'; // Terminate the string to get the parent path
+    } else {
+        // Handle unexpected cases
+        return -EINVAL; // Invalid argument
+    }
+
     struct wfs_log_entry *parent_dir_entry = looper(parent_path, S_IFDIR);
     if (!parent_dir_entry) {
         return -ENOENT;  // Parent directory not found
     }
 
+    printf("New path: %s\n", new_path);
+    printf("Parent path: %s\n", parent_path);
     // Prepare the new file entry
     struct wfs_dentry newFile;
-    strncpy(newFile.name, last_slash + 1, MAX_FILE_NAME_LEN);
+    strncpy(newFile.name, new_path, MAX_FILE_NAME_LEN);
     newFile.name[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
-    newFile.inode_number = global_superblock->head + sizeof(struct wfs_log_entry); // Assign inode number after new dir entry
+    newInode += 1;
+    newFile.inode_number = newInode; // Assign inode number after new dir entry
+    printf("Created new dentry with name %s\n", newFile.name);
 
     // Create a new log entry for the parent directory
     struct wfs_log_entry *newParentDirEntry = (struct wfs_log_entry *)((char *)global_superblock + global_superblock->head);
@@ -171,7 +250,6 @@ static int wfs_mknod(const char *path, mode_t mode, dev_t rdev) {
     // Create a new log entry for the file
     struct wfs_log_entry *newFileEntry = (struct wfs_log_entry *)((char *)global_superblock + global_superblock->head);
     newFileEntry->inode.mode = S_IFREG | mode;
-    newInode += 1;
     newFileEntry->inode.inode_number = newInode;
     newFileEntry->inode.links = 1;
     newFileEntry->inode.uid = getuid();
@@ -192,11 +270,13 @@ static int wfs_mknod(const char *path, mode_t mode, dev_t rdev) {
         return -EIO;
     }
 
+    printf("Reached end of mknod for path: %s\n", path);
     return 0;
 }
 
 
 static int wfs_mkdir(const char *path, mode_t mode) {
+    printf("Entering mkdir path: %s *****************************\n", path);
     // Check if superblock is properly mapped
     if (!global_superblock) {
         fprintf(stderr, "Superblock not mapped\n");
@@ -204,23 +284,53 @@ static int wfs_mkdir(const char *path, mode_t mode) {
     }
 
     // Use looper to find the parent directory of the path
-    char parent_path[MAX_PATH_LEN];
-    strcpy(parent_path, path);
-    char *last_slash = strrchr(parent_path, '/');
-    if (last_slash == NULL) {
-        return -ENOENT;  // Invalid path
+    char parent_dir[MAX_PATH_LEN];
+
+    if (strcmp(path, "/") == 0) {
+        // Path is the root directory, no parent
+        return -EISDIR;
     }
-    *last_slash = '\0'; // Terminate the string to get the parent path
-    struct wfs_log_entry *parent_dir_entry = looper(parent_path, S_IFDIR);
+
+    strcpy(parent_dir, path);
+    char *last_slash = strrchr(parent_dir, '/');
+
+    char new_dir[MAX_FILE_NAME_LEN];
+
+    if (last_slash != NULL) {
+        // Copy everything after the last slash to new_path
+        strncpy(new_dir, last_slash + 1, MAX_FILE_NAME_LEN); // Copy up to 4 characters to leave room for the null terminator
+        new_dir[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
+    } else {
+        // If there's no slash, the whole path is the new path
+        strncpy(new_dir, path, MAX_FILE_NAME_LEN);
+        new_dir[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
+    }
+
+    if (last_slash == parent_dir) {
+        // If the path is a direct child of root (e.g., "/a")
+        strcpy(parent_dir, "/");
+    } else if (last_slash != NULL) {
+        // For deeper paths, find the parent directory
+        *last_slash = '\0'; // Terminate the string to get the parent path
+    } else {
+        // Handle unexpected cases
+        return -EINVAL; // Invalid argument
+    }
+
+    struct wfs_log_entry *parent_dir_entry = looper(parent_dir, S_IFDIR);
     if (!parent_dir_entry) {
         return -ENOENT;  // Parent directory not found
     }
 
-    // Prepare the new directory entry
+    printf("New path: %s\n", new_dir);
+    printf("Parent path: %s\n", parent_dir);
+    // Prepare the new file entry
     struct wfs_dentry newDir;
-    strncpy(newDir.name, last_slash + 1, MAX_FILE_NAME_LEN);
+    strncpy(newDir.name, new_dir, MAX_FILE_NAME_LEN);
     newDir.name[MAX_FILE_NAME_LEN - 1] = '\0'; // Ensure null-termination
-    newDir.inode_number = global_superblock->head + sizeof(struct wfs_log_entry); // Assign inode number after new dir entry
+    newInode += 1;
+    newDir.inode_number = newInode; // Assign inode number after new dir entry
+    printf("Created new dentry with name %s\n", newDir.name);
 
     // Create a new log entry for the parent directory
     struct wfs_log_entry *newParentDirEntry = (struct wfs_log_entry *)((char *)global_superblock + global_superblock->head);
@@ -233,7 +343,6 @@ static int wfs_mkdir(const char *path, mode_t mode) {
     // Create the new log entry for the directory
     struct wfs_log_entry *newDirEntry = (struct wfs_log_entry *)((char *)global_superblock + global_superblock->head);
     newDirEntry->inode.mode = S_IFDIR | mode;  // Ensure the mode indicates a directory
-    newInode += 1;
     newDirEntry->inode.inode_number = newInode;
     newDirEntry->inode.links = 2; // Directories typically have 2 links (".", "..")
     newDirEntry->inode.uid = getuid();
@@ -254,12 +363,14 @@ static int wfs_mkdir(const char *path, mode_t mode) {
         return -EIO;
     }
 
+    printf("Reached end of mkdir for path: %s\n", path);
     return 0;
 }
 
 static int wfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
     // Use looper to find the log entry for the file
     struct wfs_log_entry *file_entry = looper(path, 0);
+    printf("Read path: %s", path);
     if (!file_entry) {
         return -ENOENT; // File not found
     }
